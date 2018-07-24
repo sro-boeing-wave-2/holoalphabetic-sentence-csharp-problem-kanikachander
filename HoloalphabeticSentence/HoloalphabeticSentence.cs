85a9a332c2b3d47fbc35b8f01d0b6101b201004b
Kanika <kanikachander1@gmail.com>
2018-07-24 15:51:49 +0530

    Removed for loop and used .All linq

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,16 +8,18 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            bool check=true;
-            string str = input.ToLower();
-            for(int i=0; i<26; i++)
-            {
-                if (str.Contains(alphabets[i]) == false)
-                {
-                    return check = false;
-                }
-            }
-            return check;   
+            string inputInLowerCase = input.ToLower();
+            return alphabets.All(c => inputInLowerCase.Contains(c));
+            //bool check = true;
+            //string str = input.ToLower();
+            //for (int i = 0; i < 26; i++)
+            //{
+            //    if (str.Contains(alphabets[i]) == false)
+            //    {
+            //        return check = false;
+            //    }
+            //}
+            //return check;   
         }
     }
 }
